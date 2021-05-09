8a99d60444549e6b53ba9b68b5b05e36e9ac480e
jose <josemancillamarambio@gmail.com>
2021-05-09 16:13:49 -0400

    comentarios

--- Proyecto/src/Main.java
+++ Proyecto/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class Main{
     public static void main(String[] args) {
-        boolean continua ;
+        boolean continua ; //Condicion del ciclo while
 
         Scanner scanner = new Scanner(System.in);
 
@@ -13,26 +13,26 @@ class Main{
 
         do {
             System.out.println("Ingrese 1 si quiere ver los datos de los Vehiculos disponibles: ");
-            try {
-                continua = false;
-                int menu = scanner.nextInt();
+            try { //bloque de codigo por si el usurio se equivoca no genera error en la pantalla
+                continua = false; //Si continua es falso se detiene el ciclo while
+                int menu = scanner.nextInt(); //creando leyendo el objeto que lee la entrada por teclado.
 
-                if (menu == 1) {
-                    camion_1.imprimir();
+                if (menu == 1) { // condicion if para generar el menu
+                    camion_1.imprimir(); //llamando a la funcion imprimir definida en Vehiculo
                     // bus_1.imprimir();
                 }
-                else {
-                    System.out.println("Valor no valido");
-                    continua = true;
+                else { //si el ingresa un numero pero no el requerido entra a este bloque de codigo
+                    System.out.println("Valor no valido"); //imprime una alerta
+                    continua = true; // continua es verdadera para que el ciclo no se corte y pregunte nuevamente el numero
                 }
 
-            } catch (java.util.InputMismatchException e) {
-                System.out.println("Valor no valido, intentelo de nuevo");
+            } catch (java.util.InputMismatchException e) { //bloque de codigo por si algo falla en try
+                System.out.println("Valor no valido, intentelo de nuevo"); //imprime una alerta
                 scanner.next();
-                continua = true;
+                continua = true; //continua queda verdadero para que el que el ciclo no se corte
 
             }
-        }while (continua);
+        }while (continua); // verifica si continua en false
 
 
     }
