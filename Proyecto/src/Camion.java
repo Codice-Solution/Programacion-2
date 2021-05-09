140aee49bd16b10c17f4a31873cd1442f7e3261b
Miguel-AMF <miguel.maturana@hotmial.com>
2021-05-08 22:26:57 -0400

    Agrego clases Camion, Carretara y Conductor

---
+++ Proyecto/src/Camion.java
@@ -0,0 +1,31 @@
+public class Camion extends Vehiculo{
+    private int capacidad_de_carga;
+    private Gps gps;
+
+    public Camion(Conductor driver, String patente, int masa, int altura){
+        super(driver, patente, masa, altura);
+
+    }
+
+    public int getCapacidad_de_carga() {
+        return capacidad_de_carga;
+    }
+
+    public void setCapacidad_de_carga(int capacidad_de_carga) {
+        this.capacidad_de_carga = capacidad_de_carga;
+    }
+
+    public Gps getGps() {
+        return gps;
+    }
+
+    public void setGps(Gps gps) {
+        this.gps = gps;
+    }
+
+    public void imprimir(){
+        System.out.println("Nombre del conductor: " + driver.getNombre() + ", Rut: "+ driver.getRut() + ", Edad: " + driver.getEdad() +
+                ", Patente del vehiculo: " + getPatente() + ", Masa: " + getMasa() + ", Altura: " + getAltura());
+    }
+
+}
---
+++ Proyecto/src/Carretera.java
@@ -0,0 +1,7 @@
+class Carretera { //Superclase
+    private int masa_maxima; //Masa maxima que soporta la carretera.
+    private int velocidad_maxima; //Velocidad maxima que permite la carretera.
+    private boolean tunel; // si hay tunel o no hay tunel.
+    private int altura_maxima; //altura maxima que permite la cerretara.
+
+}
