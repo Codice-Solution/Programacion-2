b8e561c62359011ea1f598ff84c16760494d4244
Migue-MF <miguel.maturana@hotmail.com>
2021-05-13 15:46:40 -0400

    cambio eventos

--- Proyecto/src/Eventos.java
+++ Proyecto/src/Eventos.java
@@ -3,7 +3,7 @@
 // Autores
 // Jose Mancilla Marambio ; 20.476.565-0 ; jose.mancilla@alumnos.uv.cl
 // Miguel Maturana Figueroa ; 18.999.258-0 ; miguel.maturanaf@alumnos.un.cl
-
+//
 class Eventos {
     private int vel_max;
 
