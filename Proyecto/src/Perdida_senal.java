f822dd0387a7156d6766ac249a79e7852aeb6ffb
Migue-MF <miguel.maturana@hotmail.com>
2021-05-13 15:57:03 -0400

    agrego atributo y metodo a clase Perdida_senal

--- Proyecto/src/Perdida_senal.java
+++ Proyecto/src/Perdida_senal.java
@@ -1,2 +1,13 @@
-class Perdida_senal extends Eventos{
+class Perdida_senal extends Eventos {
+
+    private boolean conexion;
+
+    public void imprimir_advertencia() {
+
+        if (conexion == true) {
+            System.out.println("conexion estable"); }
+
+        else {
+            System.out.println("la senal de ha perdido"); }
+    }
 }
