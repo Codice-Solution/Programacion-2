62ac3ab9d024dc80b9383df91e01daa695210150
Miguel Andrés Maturana Figueroa <76232130+Migue-AMF@users.noreply.github.com>
2021-05-14 23:06:46 -0400

    Comentarios

--- Proyecto/src/Perdida_senal.java
+++ Proyecto/src/Perdida_senal.java
@@ -1,3 +1,9 @@
+// Link repositorio Github https://github.com/Codice-Solution/Programacion-2/tree/Entrega_1/Proyecto
+// Codificado en Intellij Idea
+// Autores
+// Jose Mancilla Marambio ; 20.476.565-0 ; jose.mancilla@alumnos.uv.cl
+// Miguel Maturana Figueroa ; 18.999.258-0 ; miguel.maturanaf@alumnos.un.cl
+
 class Perdida_senal extends Eventos {
 
     private boolean conexion;
