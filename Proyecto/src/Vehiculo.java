1b08df40d6c5ae924c7d670c24214cbaf16904ae
Jose Mancilla <josemancillamarambio@gmail.com>
2021-06-14 19:36:27 -0400

    clase abstracta

--- Proyecto/src/Vehiculo.java
+++ Proyecto/src/Vehiculo.java
@@ -4,7 +4,7 @@
 // Jose Mancilla Marambio ; 20.476.565-0 ; jose.mancilla@alumnos.uv.cl
 // Miguel Maturana Figueroa ; 18.999.258-0 ; miguel.maturanaf@alumnos.un.cl
 
-public class Vehiculo { //Super Clase
+abstract class Vehiculo { //Super Clase
     private String patente; //Identificador de cada vehiculo.
     private String tipo;
     private int masa; //Masa actual del vehiculo.
